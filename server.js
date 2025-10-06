afa7ab3544d092d82dced764fe31b36b6ca43953
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-06 21:05:56 +0300

    Fix German spelling error in welcome message

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Willkommen im Versionskontrolle Wiki!');
+    res.send('Willkommen im Versionskontroll-Wiki!');
 });
 
 app.listen(PORT, () => {
